3ab0aad2e88e4d518f6dc73fd775831100cb19ef
Samuel Schlesinger <sgschlesinger@gmail.com>
2021-12-07 18:14:52 -0500

    First commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+const HELLO: &[u8] = b"Hello, world!";
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    test_main();
+
+    println!("Hello, world: {}", "Boo Ya Yoo Ya");
+
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
